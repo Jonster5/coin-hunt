3fb3e961044f2d8b9ba30c4c99518810c25fa500
Jonster5 <onlyaccelerating@gmail.com>
2024-06-06 21:27:55 -0400

    fix texture

--- src/init.ts
+++ src/init.ts
@@ -26,37 +26,37 @@ function setupCamera(r: Raxis) {
 }
 
 async function loadTextures(path: string, length: number): Promise<HTMLImageElement[]> {
-	const images: HTMLImageElement[] = []
-	for (let i = 0; i < length; i++) {
-		const src = path + '/' + i.toString().padStart(4, '0') + '.png'
-		const image = new Image()
-		image.onload = () => { 
-			images.push(image);
-		}
-		image.src = src
-	}
-	return images
+	const pending_images = Array.from({ length }, (_, i) => path + '/' + i.toString().padStart(4, '0') + '.png').map(
+		(path) =>
+			new Promise<HTMLImageElement>((resolve) => {
+				const img = new Image();
+				img.src = path;
+				img.onload = () => resolve(img);
+			})
+	);
+
+	return await Promise.all(pending_images);
 }
 
 async function loadAssets(r: Raxis) {
 	const renderer = r.query(CanvasRenderer, Tagged(RendererTag)).expectSingle();
 	const map_image = new OffscreenCanvas(2500, 2500);
-	const assets = r.global(GameAssets); 
+	const assets = r.global(GameAssets);
 
 	assets['coin_hunter'] = await load(renderer, ['coin_hunter.png']);
-	assets['arrow'] = await load(renderer, ['arrow.png'])
-	assets['lava'] = await load(renderer, ['Textures/River/0004.png'])
+	assets['arrow'] = await load(renderer, ['arrow.png']);
+	assets['lava'] = await load(renderer, ['Textures/River/0004.png']);
 	assets['map_image'] = await load(renderer, map_image);
-	assets['map_canvas'] = map_image
-	assets['textures']['dirt'] = await loadTextures('Textures/Dirt', 10)
-	assets['textures']['bricks_floor'] = await loadTextures('Textures/Floor Bricks', 10)
-	assets['textures']['grass'] = await loadTextures('Textures/Grass', 10)
-	assets['textures']['lake'] = await loadTextures('Textures/Lake', 40)
-	assets['textures']['leaves'] = await loadTextures('Textures/Leaves', 10)
-	assets['textures']['river'] = await loadTextures('Textures/River', 40)
-	assets['textures']['stone_floor'] = await loadTextures('Textures/Stone Floor', 10)
-	assets['textures']['stone_wall'] = await loadTextures('Textures/Stone Wall', 10)
-	assets['textures']['bricks_wall'] = await loadTextures('Textures/Wall Bricks', 10)
+	assets['map_canvas'] = map_image;
+	assets['textures']['dirt'] = await loadTextures('Textures/Dirt', 10);
+	assets['textures']['bricks_floor'] = await loadTextures('Textures/Floor Bricks', 10);
+	assets['textures']['grass'] = await loadTextures('Textures/Grass', 10);
+	assets['textures']['lake'] = await loadTextures('Textures/Lake', 40);
+	assets['textures']['leaves'] = await loadTextures('Textures/Leaves', 10);
+	assets['textures']['river'] = await loadTextures('Textures/River', 40);
+	assets['textures']['stone_floor'] = await loadTextures('Textures/Stone Floor', 10);
+	assets['textures']['stone_wall'] = await loadTextures('Textures/Stone Wall', 10);
+	assets['textures']['bricks_wall'] = await loadTextures('Textures/Wall Bricks', 10);
 
 	r.dispatch(new CreateNextLevelSignal());
 }
--- src/level.ts
+++ src/level.ts
@@ -3,7 +3,6 @@ import { MovePlayerSignal, SpawnPlayerSignal } from './player';
 import { GameAssets } from './assets';
 import { LavaPulseSignal } from './lava';
 
-
 export class LevelDifficultyTracker {
 	difficulty = 1;
 	seed = 1;
@@ -12,7 +11,7 @@ export class LevelDifficultyTracker {
 export class LevelMusicSync {
 	tempo = 120;
 	bpm = 4;
-	current_beat: 0
+	current_beat: 0;
 }
 
 export class LevelSprite {}
@@ -20,26 +19,26 @@ export class LevelSprite {}
 export class Level {}
 
 export class CollisionMap {
-	map = new Array(50).fill(0).map(() => new Array(50).fill(0))
+	map = new Array(50).fill(0).map(() => new Array(50).fill(0));
 }
 
 export class ClearLevelSignal {}
 export class CreateNextLevelSignal {}
 
 interface PerlinSettings {
-    scale: number;
-	rotation: number
+	scale: number;
+	rotation: number;
 
 	seed: number;
-	octaves: number
-	lacunarity: number
-	contribution_factor: number
+	octaves: number;
+	lacunarity: number;
+	contribution_factor: number;
 }
 
 export class PerlinNoise implements PerlinSettings {
-    scale = 0.07;
+	scale = 0.07;
 	rotation = 0;
-	
+
 	seed = 0;
 	octaves = 4;
 	lacunarity = 0.6;
@@ -47,54 +46,61 @@ export class PerlinNoise implements PerlinSettings {
 }
 
 const sserp = (a: number, b: number, x: number) => (b - a) * ((x * (x * 6 - 15) + 10) * x * x * x) + a;
-const randomUnit = (uv: Vec2, seed: number) => new Vec2(Math.cos(((Math.sin(uv.dot(new Vec2(12.9898,78.233))+seed)*43758.5453123) % 1) * Math.PI * 2), Math.sin(((Math.sin(uv.dot(new Vec2(12.9898,78.233))+seed)*43758.5453123) % 1) * Math.PI * 2))
+const randomUnit = (uv: Vec2, seed: number) =>
+	new Vec2(
+		Math.cos(((Math.sin(uv.dot(new Vec2(12.9898, 78.233)) + seed) * 43758.5453123) % 1) * Math.PI * 2),
+		Math.sin(((Math.sin(uv.dot(new Vec2(12.9898, 78.233)) + seed) * 43758.5453123) % 1) * Math.PI * 2)
+	);
 const mod = (n: number, d: number) => ((n % d) + d) % d;
 
 function rotate(uv: Vec2, theta: number, noise: PerlinNoise) {
-    const center = uv.clone().sub(noise.scale/2);
-    return new Vec2(center.x * Math.cos(theta) - center.y * Math.sin(theta), center.x * Math.sin(theta) + center.y * Math.cos(theta));
+	const center = uv.clone().sub(noise.scale / 2);
+	return new Vec2(
+		center.x * Math.cos(theta) - center.y * Math.sin(theta),
+		center.x * Math.sin(theta) + center.y * Math.cos(theta)
+	);
 }
 
 function getAt(uv: Vec2, seed: number) {
-    const pos = uv.clone()
-    const TL = pos.clone().floor()
-    const TR = new Vec2(Math.ceil(pos.x), Math.floor(pos.y))
-    const BL = new Vec2(Math.floor(pos.x), Math.ceil(pos.y))
-    const BR = pos.clone().ceil()
+	const pos = uv.clone();
+	const TL = pos.clone().floor();
+	const TR = new Vec2(Math.ceil(pos.x), Math.floor(pos.y));
+	const BL = new Vec2(Math.floor(pos.x), Math.ceil(pos.y));
+	const BR = pos.clone().ceil();
 
-    const t = sserp(
-        randomUnit(TL, seed).dot(pos.clone().sub(TL)),
-        randomUnit(TR, seed).dot(pos.clone().sub(TR)),
-        mod(pos.x, 1)
-    )
+	const t = sserp(
+		randomUnit(TL, seed).dot(pos.clone().sub(TL)),
+		randomUnit(TR, seed).dot(pos.clone().sub(TR)),
+		mod(pos.x, 1)
+	);
 
-    const b = sserp(
-        randomUnit(BL, seed).dot(pos.clone().sub(BL)),
-        randomUnit(BR, seed).dot(pos.clone().sub(BR)),
-        mod(pos.x, 1)
-    )
+	const b = sserp(
+		randomUnit(BL, seed).dot(pos.clone().sub(BL)),
+		randomUnit(BR, seed).dot(pos.clone().sub(BR)),
+		mod(pos.x, 1)
+	);
 
-    return sserp(t, b, mod(pos.y, 1))+0.5
+	return sserp(t, b, mod(pos.y, 1)) + 0.5;
 }
 
 function fbm(noise: PerlinNoise, uv: Vec2) {
-    let pos = rotate(uv.clone().mul(noise.scale), noise.rotation, noise)
-    let sum = 0;
-    let contribution = 0.5;
-    for (let i = 0; i < noise.octaves; i++) {
-        sum += getAt(pos, noise.seed) * contribution;
-        pos = pos.div(noise.lacunarity);
-        contribution *= noise.contribution_factor
-        pos = rotate(pos, 1.2, noise)
-    }
+	let pos = rotate(uv.clone().mul(noise.scale), noise.rotation, noise);
+	let sum = 0;
+	let contribution = 0.5;
+	for (let i = 0; i < noise.octaves; i++) {
+		sum += getAt(pos, noise.seed) * contribution;
+		pos = pos.div(noise.lacunarity);
+		contribution *= noise.contribution_factor;
+		pos = rotate(pos, 1.2, noise);
+	}
 
-    return sum
+	return sum;
 }
 
 function createNextLevel(r: Raxis) {
 	if (r.noneAvailableOf(CreateNextLevelSignal)) return;
 
-	const assets = r.global(GameAssets)
+	const assets = r.global(GameAssets);
 
 	r.spawn(
 		new Level(),
@@ -102,28 +108,28 @@ function createNextLevel(r: Raxis) {
 		new LevelMusicSync(),
 		SpriteBundle({
 			texture: new Texture(assets.map_image),
-			transform: new Transform( {layer: -2, size: new Vec2(2500, 2500), translation: new Vec2(25, 25)})
+			transform: new Transform({ layer: -2, size: new Vec2(2500, 2500), translation: new Vec2(25, 25) }),
 		})
-	)
+	);
 
-	const ctx = assets['map_canvas'].getContext('2d')
+	const ctx = assets['map_canvas'].getContext('2d');
 
 	for (let i = 0; i < 50; i++) {
 		for (let j = 0; j < 50; j++) {
-			let val = fbm(r.global(PerlinNoise), new Vec2(j,i));
+			let val = fbm(r.global(PerlinNoise), new Vec2(j, i));
 			if (val < 0.33) {
-				ctx.drawImage(assets['textures']['lake'][Math.floor(Math.random() * 10) * 4], 50*j,50*i,50,50)
+				ctx.drawImage(assets['textures']['lake'][Math.floor(Math.random() * 10) * 4], 50 * j, 50 * i, 50, 50);
 			} else if (Math.random() < 0.1) {
-				ctx.drawImage(assets['textures']['leaves'][Math.floor(Math.random() * 10)], 50*j,50*i,50,50)
-				r.global(CollisionMap).map[i][j] = 1
+				ctx.drawImage(assets['textures']['leaves'][Math.floor(Math.random() * 10)], 50 * j, 50 * i, 50, 50);
+				r.global(CollisionMap).map[i][j] = 1;
 			} else {
-				console.log((assets['textures']['grass']))
-				ctx.drawImage(assets['textures']['grass'][Math.floor(Math.random() * 10)], 50*j,50*i,50,50)
+				const asset = assets['textures']['grass'][Math.floor(Math.random() * 10)];
+				ctx.drawImage(asset, 50 * j, 50 * i, 50, 50);
 				//return '#00' + Math.floor(300 - val * 256).toString(16).padStart(2, '0') + '00'
 			}
 		}
 	}
-	
+
 	r.dispatch(new SpawnPlayerSignal());
 }
 
@@ -134,12 +140,11 @@ function MusicTick(r: Raxis) {
 	level_music.current_beat++;
 	if (level_music.current_beat == level_music.bpm) level_music.current_beat = 0;
 
-	r.dispatch(new MovePlayerSignal)
-	r.dispatch(new LavaPulseSignal)
+	r.dispatch(new MovePlayerSignal());
+	r.dispatch(new LavaPulseSignal());
 
-	SystemTimer.set(r, 1000 * 60 / level_music.tempo)
+	SystemTimer.set(r, (1000 * 60) / level_music.tempo);
 }
-
 
 export const LevelPlugin = new Raxis.Builder()
 	.useComponent(LevelSprite)
